13c3aedd831c94dda72dc058da9612ece44f96fe
Kristian Bak <blaaman@gmail.com>
2016-04-09 00:29:38 +0200

    added main scene with 4 cubes that roll around weirdly"

---
+++ Leaky Boosters/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int playerNum;
+	public float acceleration = 1;
+	public ForceMode forcemode;
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		InputDevice inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+		if (inputDevice == null) return;
+		rigidbody.AddForce(new Vector3(acceleration * Time.deltaTime * inputDevice.LeftStickX, 0, acceleration * Time.deltaTime * inputDevice.LeftStickY), forcemode);
+
+	}
+}
