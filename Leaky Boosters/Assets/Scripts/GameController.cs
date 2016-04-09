06a7611cea0176a78767222e167f799cd7923000
Alexander Arendttorp <alexanderarendttorp@gmail.com>
2016-04-09 10:37:51 +0200

    Added GameController. Restart game on 'R'. Added Physics material for Bounce on ball and player.

---
+++ Leaky Boosters/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else if (Instance != null)
+			Destroy (this.gameObject);
+
+		DontDestroyOnLoad (this);
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			RestartLevel();
+		}
+	}
+
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
