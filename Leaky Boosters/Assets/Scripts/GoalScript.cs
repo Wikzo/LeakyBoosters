0107fee99a47a67d95c223767cb1bd7f123978a5
Alexander Arendttorp <alexanderarendttorp@gmail.com>
2016-04-09 12:39:36 +0200

    A whole bunch of stuff.

---
+++ Leaky Boosters/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	Renderer ren;
+	Color normalCol = new Color(0.5f, 0.5f, 0.5f);
+	Color goalCol = new Color(0, 0, 1);
+	// Use this for initialization
+	void Start () {
+		ren = GetComponent<Renderer> ();
+		ren.material.color = normalCol;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Ball") ) 
+		{
+			print ("you scored");
+			ren.material.color = goalCol;
+			StartCoroutine (resetColor ());
+		}
+	}
+
+
+	IEnumerator resetColor()
+	{
+		yield return new WaitForSeconds (2f);
+		ren.material.color = normalCol;
+	}
+}
---
+++ Leaky Boosters/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	[SerializeField]
+	private int playerIndex; 
+
+
+
+	public int GetPlayerIndex()
+	{
+		return playerIndex;
+	}
+		
+}
