ec9ccd38470339444a7aea568e9672ebfa6cf695
Kristian Bak <blaaman@gmail.com>
2016-04-10 10:56:28 +0200

    intro scene is set up visually, tweaked some visuals

---
+++ Leaky Boosters/Assets/IntroBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class IntroBehaviour : MonoBehaviour {
+
+	public Transform ball;
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.transform.DOLocalMoveZ(-10f, 8, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+		Camera.main.transform.DOLocalMoveY(1.39f, 5, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+		ball.DOLocalMoveY(4.6f, 10, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ball.eulerAngles += Vector3.up * Time.deltaTime * 6;
+	}
+}
